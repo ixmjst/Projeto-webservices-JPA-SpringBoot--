558364cfca2c797828a81bf222f83db92d8dc3fe
IXMJST <tjosesimao@gmail.com>
2026-03-03 02:31:38 +0100

    Many-To-Many assossition  with JoinTable

--- src/main/java/javatest/entities/Category.java
+++ src/main/java/javatest/entities/Category.java
@@ -2,12 +2,16 @@ package javatest.entities;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.Objects;
 import java.io.Serializable;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_category")
@@ -17,8 +21,8 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
-	
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
 	private Set <Product> products = new HashSet();
 	
 	public Category() {
--- src/main/java/javatest/entities/Product.java
+++ src/main/java/javatest/entities/Product.java
@@ -11,8 +11,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-
+import jakarta.persistence.JoinColumn;
 @Entity	
 @Table(name = "tb_product")
 public class Product implements Serializable {
@@ -25,7 +27,8 @@ private Double price;
 private String description;
 private String imgUrl;
 
-
+@ManyToMany
+@JoinTable (name = "tb_product_category",joinColumns = @JoinColumn(name = "product_id"),inverseJoinColumns = @JoinColumn(name = "category_id"))
 private Set <Category> categories = new HashSet();
 
 public Product() {}
