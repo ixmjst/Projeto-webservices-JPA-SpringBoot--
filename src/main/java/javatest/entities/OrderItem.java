426f461908f13b1033d79fcaee88f4743b8c92f1
IXMJST <tjosesimao@gmail.com>
2026-03-06 02:50:35 +0100

    fix

--- src/main/java/javatest/entities/OrderItem.java
+++ src/main/java/javatest/entities/OrderItem.java
@@ -10,7 +10,7 @@ import javatest.entities.pk.OrderItemPK;
 public class OrderItem implements java.io.Serializable {
 	private static final long serialVersionUID = 1L;
 @EmbeddedId
-private OrderItemPK id ;
+private OrderItemPK id = new OrderItemPK();
 private Integer quantity;
 private Double price;
 public OrderItem() {
